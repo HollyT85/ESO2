3529ffe589ad05db3a5733ed165b249354971a60
HollyT85 <h.e.turner@gmail.com>
2025-12-08 09:02:55 +0000

    mission pages updated

--- mission/urls.py
+++ mission/urls.py
@@ -7,5 +7,8 @@ from . import views
 app_name = 'mission'
 
 urlpatterns = [
-    path('', views.mission, name='mission'),
+    path('', views.plan, name='plan'),
+    path('mission/', views.mission, name='mission'),
+    path('work/', views.work, name='work'),
+    path('gallery/', views.gallery, name='gallery'),
 ]
--- mission/views.py
+++ mission/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 
+def plan(request):
+    """Mission page view"""
+    return render(request, 'mission/plan.html')
+
 def mission(request):
     """Mission page view"""
     return render(request, 'mission/mission.html')
+
+def work(request):
+    """Mission page view"""
+    return render(request, 'mission/work.html')
+
+"""Mission page view"""
+    return render(request, 'mission/gallery.html')
