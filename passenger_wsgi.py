102f9aa00194ef2f6c4a3ac278022eeb848f0b35
HollyT85 <92177170+HollyT85@users.noreply.github.com>
2026-04-12 08:17:20 +0100

    Update WSGI configuration for Django application

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,10 @@
 import os
 import sys
 
-sys.path.insert(0, '/home/earthsci/public_html/r/ESO2')
+sys.path.insert(0, '/home/earthsci/public_html/ESO2')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESO.settings')
 
-from ESO.wsgi import application as app
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
