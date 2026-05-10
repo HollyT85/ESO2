45965184c8595fde5fec8b9d43962d742ef49207
HollyT85 <92177170+HollyT85@users.noreply.github.com>
2026-05-10 18:15:48 +0000

    ESO_reset

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, '/home/earthsci/public_html/ESO')
+sys.path.insert(0, '/home/earthsci/public_html/ESO_reset')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ESO.settings')
 
