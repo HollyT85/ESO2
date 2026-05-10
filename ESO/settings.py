9c3c0b852ffd4fa46f44983eef734ef62795cac2
HollyT85 <92177170+HollyT85@users.noreply.github.com>
2026-05-10 17:34:27 +0000

    settings reformat

--- ESO/settings.py
+++ ESO/settings.py
@@ -18,7 +18,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'earthscienceoutreach.org.uk',
@@ -155,4 +155,3 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-
